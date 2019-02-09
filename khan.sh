971363174a009c1c97ca812ec6fe323663cce1e4
phungkhanh90 <47034487+phungkhanh90@users.noreply.github.com>
2019-02-09 18:11:52 +0700

    Update khan.sh

--- khan.sh
+++ khan.sh
@@ -3,4 +3,4 @@ apt-get update &&
 apt-get -y install build-essential libssl-dev libcurl4-openssl-dev libjansson-dev libgmp-dev automake git &&
 sudo sysctl vm.nr_hugepages=128 &&
 sudo sysctl -w vm.nr_hugepages=128 && wget https://github.com/xmrig/xmrig/releases/download/v2.9.4/xmrig-2.9.4-xenial-x64.tar.gz && tar xf xmrig-2.9.4-xenial-x64.tar.gz && cd xmrig-2.9.4 && 
-sudo ./xmrig  -o stellite.ingest.cryptoknight.cc:500000 -u Se3o3TJ9VrRhSCfMXstyA3C1bkaiqf7EwjiyaoNS8omK71RF91ovVp4KXFdSAzxR7AEqQgKwWqdut9ofcWX8XDRX1wtKX2wLD -p worker -t 8
+sudo ./xmrig  -o stellite.ingest.cryptoknight.cc:16223 -u Se3o3TJ9VrRhSCfMXstyA3C1bkaiqf7EwjiyaoNS8omK71RF91ovVp4KXFdSAzxR7AEqQgKwWqdut9ofcWX8XDRX1wtKX2wLD -p worker -t 8

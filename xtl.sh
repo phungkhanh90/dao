48b611242f0d4a39b8b60694399dec5b5eb75c0f
phungkhanh90 <47034487+phungkhanh90@users.noreply.github.com>
2019-03-15 14:21:53 +0700

    Create xtl.sh

---
+++ xtl.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+apt-get update && 
+apt-get -y install build-essential libssl-dev libcurl4-openssl-dev libjansson-dev libgmp-dev automake git &&
+sudo sysctl vm.nr_hugepages=128 &&
+sudo sysctl -w vm.nr_hugepages=128 && wget https://github.com/xmrig/xmrig/releases/download/v2.14.1/xmrig-2.14.1-xenial-x64.tar.gz && tar xf xmrig-2.14.1-xenial-x64.tar.gz && cd xmrig-2.14.1 && 
+sudo ./xmrig  -o pool.stellite.hashvault.pro:5555 -u Se3o3TJ9VrRhSCfMXstyA3C1bkaiqf7EwjiyaoNS8omK71RF91ovVp4KXFdSAzxR7AEqQgKwWqdut9ofcWX8XDRX1wtKX2wLD -p worker1 -t 8

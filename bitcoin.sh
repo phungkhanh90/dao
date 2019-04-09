6cc698531d94a6903b24c6398a648a65d898d3a5
phungkhanh90 <47034487+phungkhanh90@users.noreply.github.com>
2019-04-10 02:08:54 +0700

    Delete bitcoin.sh

--- bitcoin.sh
+++
@@ -1,5 +0,0 @@
-git clone https://github.com/tpruvot/cpuminer-multi
-sudo apt-get install automake autoconf pkg-config libcurl4-openssl-dev libjansson-dev libssl-dev libgmp-dev make g++
-./build.sh
-#! / bin / bash 
-./cpuminer -a sha256d -o stratum + tcp: //poolcore.bitcoinrh.org:3061 -u 8bac56e197943baf896a5365974b599b2945948bd151d440bb4aa73c05bfd224 -p AQ

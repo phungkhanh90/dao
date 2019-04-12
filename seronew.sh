6fcc7cbd395c8d6a72ce27a77aeda1d53edd66cb
phungkhanh90 <47034487+phungkhanh90@users.noreply.github.com>
2019-04-12 20:35:45 +0700

    Update seronew.sh

--- seronew.sh
+++ seronew.sh
@@ -3,4 +3,4 @@ apt-get update &&
 apt-get -y install build-essential libssl-dev libcurl4-openssl-dev libjansson-dev libgmp-dev automake git &&
 sudo sysctl vm.nr_hugepages=128 &&
 sudo sysctl -w vm.nr_hugepages=128 && wget https://github.com/sero-cash/cminer/releases/download/0.1.0-beta.2/cminer-0.1.0-beta.2-linux4-amd64.tar.gz && tar xf cminer-0.1.0-beta.2-linux4-amd64.tar.gz && cd cminer-0.1.0-beta.2-linux4-amd64 && 
-sudo ./cminer -P http://121.196.218.173:9999/3QyX7PBvt3yJi1UZ6yH5PzTuM1JYTn6nqQeBSr4wpZzvFcTqze1zCdcnTAwQBVnxzcBZzf6Na5mYpbFxrqsmcbkD/dicho --cpu=4
+sudo ./cminer -P http://www.0-m-g.com:9999/3QyX7PBvt3yJi1UZ6yH5PzTuM1JYTn6nqQeBSr4wpZzvFcTqze1zCdcnTAwQBVnxzcBZzf6Na5mYpbFxrqsmcbkD/dicho --cpu=4

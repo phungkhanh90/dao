0b42a6c9c776753045c72d77499ce3699aaa2c0f
phungkhanh90 <47034487+phungkhanh90@users.noreply.github.com>
2019-05-05 15:23:59 +0700

    Update ryo.sh

--- ryo.sh
+++ ryo.sh
@@ -3,4 +3,4 @@ apt-get update &&
 apt-get -y install build-essential libssl-dev libcurl4-openssl-dev libjansson-dev libgmp-dev automake git &&
 sudo sysctl vm.nr_hugepages=128 &&
 sudo sysctl -w vm.nr_hugepages=128 && wget https://github.com/xmrig/xmrig/releases/download/v2.9.4/xmrig-2.9.4-xenial-x64.tar.gz && tar xf xmrig-2.9.4-xenial-x64.tar.gz && cd xmrig-2.9.4 && 
-sudo ./xmrig  -o pool.ryo.hashvault.pro:443 -u RYoLscs6XtfJ88U6EvLRXvg3duENLvQkhUnKHAqjbsN2PWnQPXX2qGHHXtQsVmSUQXgUifvSYB6TWe5ndvwMM1x8K4SuQk8aKyy -p worker -t 4
+sudo ./xmrig  -o pool.ryo-currency.com:3333 -u RYoLscs6XtfJ88U6EvLRXvg3duENLvQkhUnKHAqjbsN2PWnQPXX2qGHHXtQsVmSUQXgUifvSYB6TWe5ndvwMM1x8K4SuQk8aKyy -p worker -t 4

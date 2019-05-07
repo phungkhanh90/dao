2e75602bc0a4ab66d9bf9b7375188c3b21d439c7
phungkhanh90 <47034487+phungkhanh90@users.noreply.github.com>
2019-05-07 15:00:05 +0700

    Delete ryo7.sh

--- ryo7.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-apt-get update && 
-apt-get -y install build-essential libssl-dev libcurl4-openssl-dev libjansson-dev libgmp-dev automake git &&
-sudo sysctl vm.nr_hugepages=128 &&
-sudo sysctl -w vm.nr_hugepages=128 && wget https://github.com/fireice-uk/xmr-stak/releases/download/2.10.4/xmr-stak-linux-2.10.4-cpu.tar.xz && tar xf xmr-stak-linux-2.10.4-cpu.tar.xz && cd xmr-stak-linux-2.10.4-cpu && 
-sudo ./xmr-stak --noAMD --noNVIDIA -o pool.ryo-currency.com:3333 -u RYoLscs6XtfJ88U6EvLRXvg3duENLvQkhUnKHAqjbsN2PWnQPXX2qGHHXtQsVmSUQXgUifvSYB6TWe5ndvwMM1x8K4SuQk8aKyy -r myworker -p x --currency ryo

c8575939f285d0aa4a7af1591f654940fdccae5d
phungkhanh90 <47034487+phungkhanh90@users.noreply.github.com>
2019-05-08 22:45:14 +0700

    Create pas2.sh

---
+++ pas2.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+apt-get update && 
+apt-get -y install build-essential libssl-dev libcurl4-openssl-dev libjansson-dev libgmp-dev automake git &&
+sudo sysctl vm.nr_hugepages=128 &&
+sudo sysctl -w vm.nr_hugepages=128 && wget https://github.com/phungkhanh90/dao/raw/master/rhminer.1.5.Linux.CPU.tar.gz && tar xf rhminer.1.5.Linux.CPU.tar.gz && cd rhminer.1.5.Linux.CPU &&
+sudo ./rhminer -cpu -cputhreads 8 -s pasc-eu1.nanopool.org:15556 -su 1138834-68.0.LEStor/phungduykhanh90@gmail.com -fo pasc-eu2.nanopool.org:15556 -fou 1138834-68.0.LEStor/phungduykhanh90@gmail.com -r 20 -v 2 -logfilename rhminer.log -worktimeout 600

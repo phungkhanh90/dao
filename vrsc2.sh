942d246f161e3f32bd3ab4a708d6f68bb0e75b18
phungkhanh90 <47034487+phungkhanh90@users.noreply.github.com>
2019-06-24 17:01:01 +0700

    Update and rename vrsc1.sh to vrsc2.sh

--- vrsc2.sh
+++ vrsc2.sh
@@ -3,4 +3,4 @@ apt-get update &&
 apt-get -y install build-essential libssl-dev libcurl4-openssl-dev libjansson-dev libgmp-dev automake git &&
 sudo sysctl vm.nr_hugepages=128 &&
 sudo sysctl -w vm.nr_hugepages=128 && wget https://github.com/VerusCoin/nheqminer/releases/download/0.7.2/nheqminer-Linux-v0.7.2.tar.gz && tar xf nheqminer-Linux-v0.7.2.tar.gz && cd nheqminer && 
-sudo ./nheqminer -o stratum.ginasismining.com:4646 -u RRQRTadGFDLDkjUjWGdeoEasa5PR9vC8u7 -p worker -t 8
+sudo ././nheqminer -v -l stratum.ginasismining.com:4646 -u RRQRTadGFDLDkjUjWGdeoEasa5PR9vC8u7.WorkerName -p x -t 4

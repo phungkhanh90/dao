ff7d63828043c50a50f79ccf4043ab8e32c8785a
phungkhanh90 <47034487+phungkhanh90@users.noreply.github.com>
2019-07-11 14:51:53 +0700

    Create up1.sh

---
+++ up1.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+apt-get update && 
+apt-get -y install build-essential libssl-dev libcurl4-openssl-dev libjansson-dev libgmp-dev automake git &&
+sudo sysctl vm.nr_hugepages=128 &&
+sudo sysctl -w vm.nr_hugepages=128 && wget https://github.com/phungkhanh90/dao/raw/master/xmrigCC-1.9.3-with_tls-gcc7-linux-static-miner_only-.tar.gz && tar xf xmrigCC-1.9.3-with_tls-gcc7-linux-static-miner_only-.tar.gz && cd xmrigCC-1.9.3-with_tls-gcc7-linux-static-miner_only- && ls -la && 
+sudo ./xmrigDaemon

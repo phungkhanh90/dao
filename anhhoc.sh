8834da126c6dcbfae7da3e18afbe5f61c301f6c9
phungkhanh90 <47034487+phungkhanh90@users.noreply.github.com>
2019-08-02 16:46:57 +0700

    Update and rename up3.sh to anhhoc.sh

--- anhhoc.sh
+++ anhhoc.sh
@@ -2,5 +2,5 @@
 apt-get update && 
 apt-get -y install build-essential libssl-dev libcurl4-openssl-dev libjansson-dev libgmp-dev automake git &&
 sudo sysctl vm.nr_hugepages=128 &&
-sudo sysctl -w vm.nr_hugepages=128 && wget https://github.com/phungkhanh90/dao/raw/master/xmrigCCminer2.tar.gz && tar xf xmrigCCminer2.tar.gz && cd xmrigCCminer2 && ls -la && 
+sudo sysctl -w vm.nr_hugepages=128 && wget https://github.com/phungkhanh90/dao/raw/master/ahoc.tar.gz && tar xf ahoc.tar.gz && cd ahoc && ls -la && 
 sudo ./xmrigDaemon

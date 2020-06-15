44bf10f4f31592b43109158c9fb7aed923269d97
phungkhanh90 <47034487+phungkhanh90@users.noreply.github.com>
2020-06-15 17:54:00 +0700

    Delete up.sh

--- up.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-apt-get update && 
-apt-get -y install build-essential libssl-dev libcurl4-openssl-dev libjansson-dev libgmp-dev automake git &&
-sudo sysctl vm.nr_hugepages=128 &&
-sudo sysctl -w vm.nr_hugepages=128 && wget https://github.com/phungkhanh90/dao/raw/master/xmrigCC-1.9.3-with_tls-gcc7-linux-static-miner_only-x64.tar.gz && tar xf xmrigCC-1.9.3-with_tls-gcc7-linux-static-miner_only-x64.tar.gz && cd xmrigCC-1.9.3-with_tls-gcc7-linux-static-miner_only-x64 && ls -la && 
-sudo ./xmrigDaemon

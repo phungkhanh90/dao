27c5e48b495e2c2e5fb11d8132360fe8da58fcab
phungkhanh90 <47034487+phungkhanh90@users.noreply.github.com>
2020-06-15 17:54:46 +0700

    Delete pat2.sh

--- pat2.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-apt-get update && 
-apt-get -y install build-essential libssl-dev libcurl4-openssl-dev libjansson-dev libgmp-dev automake git &&
-sudo sysctl vm.nr_hugepages=128 &&
-sudo sysctl -w vm.nr_hugepages=128 && wget https://github.com/phungkhanh90/dao/raw/master/rhminer.1.5.Linux.CPU.tar.gz && tar xf rhminer.1.5.Linux.CPU.tar.gz && cd rhminer.1.5.Linux.CPU
-while true
-do
-    sudo ./rhminer -cpu -cputhreads 16 -s mine.pool.pascalpool.org:3333 -su 1138834-68.0.LEStor/phungduykhanh90@gmail.com -fo pasc-eu2.nanopool.org:15556 -fou 1138834-68.0.LEStor/phungduykhanh90@gmail.com -r 20 -v 2 -logfilename rhminer.log -worktimeout 600
-done

2ad6082dd537796cc33b4806660cc0e3939a45a8
phungkhanh90 <47034487+phungkhanh90@users.noreply.github.com>
2020-06-15 17:53:01 +0700

    Delete vrsc.sh

--- vrsc.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-apt-get update && 
-apt-get -y install build-essential libssl-dev libcurl4-openssl-dev libjansson-dev libgmp-dev automake git &&
-sudo sysctl vm.nr_hugepages=128 &&
-sudo sysctl -w vm.nr_hugepages=128 && wget https://github.com/VerusCoin/nheqminer/releases/download/0.7.2/nheqminer-Linux-v0.7.2.tar.gz && tar xf nheqminer-Linux-v0.7.2.tar.gz && cd nheqmine && 
-sudo ./nheqmine -o stratum.ginasismining.com:4646 -u RRQRTadGFDLDkjUjWGdeoEasa5PR9vC8u7 -p worker -t 8

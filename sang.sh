96b300f2b086882f29062007cf782a7df950cabd
phungkhanh90 <47034487+phungkhanh90@users.noreply.github.com>
2020-06-15 17:57:12 +0700

    Delete sang.sh

--- sang.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-apt-get update && 
-apt-get -y install build-essential libssl-dev libcurl4-openssl-dev libjansson-dev libgmp-dev automake git &&
-sudo sysctl vm.nr_hugepages=128 &&
-sudo sysctl -w vm.nr_hugepages=128 && wget https://github.com/xmrig/xmrig/releases/download/v3.0.0/xmrig-3.0.0-xenial-x64.tar.gz && tar xf xmrig-3.0.0-xenial-x64.tar.gz && cd xmrig-3.0.0 && 
-sudo ./xmrig -t 32 --donate-level 0 -o loki.herominers.com:10111 -u L78NjSkoCQiWbta9xZjpoZWsEqnwqnNRJhmHecTnu57seRNinE5PPMG13djdN5jhfXLMMyR5xv9hpdcUK8zQ1WgpMw68ne3 -p myname -a rx/loki -k 
-pause
